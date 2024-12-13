20e75d90d493c7f7c8ce546b27b37da45e95943e
Beatriz Santana <beatrizsantana1993@gmail.com>
2024-12-13 18:20:01 -0300

    adiconando icons aos steps

--- src/components/step/index.tsx
+++ src/components/step/index.tsx
@@ -1,14 +1,20 @@
 import { View, Text } from "react-native";
+import { IconProps } from "@tabler/icons-react-native";
 
 import { sStep } from "./styles";
+import { colors } from "@/styles/colors";
 
-type Props = {
-   title: string;
-   description: string;
-}
-export function Step({ title, description}: Props) {
+export type Props = {
+  title: string;
+  description: string;
+  icon: React.ComponentType<IconProps>
+};
+
+export function Step({ title, description, icon: Icon}: Props) {
   return (
     <View style={sStep.container}>
+      {Icon && <Icon size={32} color={colors.red.base} />}
+      
       <View style={sStep.details}>
         <Text style={sStep.title}>{title}</Text>
         <Text style={sStep.description}>{description}</Text>
